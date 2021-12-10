a3367765dfd12492705a8c51c8e5ec74f69590dc
Florian Müllner <flo-m@gmx.at>
2021-12-10 20:33:27 +0100

    Add first app and models

---
+++ learnmodules/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Question(models.Model):
+    def __str__(self):
+        return self.question_1
+    question_1 = models.CharField(max_length=200, default='')
+    answer_1 = models.CharField(max_length=200, default='')
+    answer_2 = models.CharField(max_length=200, default='')
+    answer_3 = models.CharField(max_length=200, default='')
+    answer_4 = models.CharField(max_length=200, default='')
