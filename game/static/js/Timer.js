a3400247666b7a65f95edbd8cb46730a5bb7f15c
walid <walid.al.aswad@stud.fh-campuswien.ac.at>
2022-01-22 21:34:18 +0100

    Timer v3.0

--- game/static/js/Timer.js
+++ game/static/js/Timer.js
@@ -1,5 +1,6 @@
 class Timer{
     //pass here the things were in html
+
     constructor(root) {
         root.innerHTML= Timer.getHTML();
 
@@ -7,32 +8,12 @@ class Timer{
             minutes: root.querySelector(".timer__part--minutes"),
             seconds: root.querySelector(".timer__part--seconds"),
             control: root.querySelector(".timer__btn--control"),
-            reset: root.querySelector(".timer__btn--reset")
         };
         //no interval so the timer will not be running but i want to trigger it with set interval
         this.interval = null;
         // this will be the seconds as 0 by default
-        this.remainingSeconds= 0;
-
-        this.element.control.addEventListener("click",() =>{
-            if (this.interval=== null){
-                this.start();
-            }
-            else {
-                this.stop();
-            }
-        });
-        this.element.reset.addEventListener("click",() =>{
-            const inputMinutes = prompt("Enter number of minutes");
-
-            if (inputMinutes < 60){
-                this.stop();
-
-                // to convert our time into seconds
-                this.remainingSeconds= inputMinutes*60;
-                this.updateInterfaceTime();
-            }
-        });
+        this.remainingSeconds= 11;
+        this.start();
     }
 
     updateInterfaceTime(){
@@ -54,27 +35,33 @@ class Timer{
             this.element.control.innerHTML= `<span class="material-icons">pause</span>`;
             this.element.control.classList.add("timer__btn--stop");
             this.element.control.classList.remove("timer__btn--start");
+
         }
 
     }
 
-    start(){
+    start() {
         //cancel current operation
-        if (this.remainingSeconds===0)
+        if (this.remainingSeconds === 0)
             return;
-
         //run code on timer in ms
         this.interval= setInterval(()=> {
             this.remainingSeconds--;
             this.updateInterfaceTime();
-
             if(this.remainingSeconds===0){
                 this.stop();
+                alert("Your time is over. \n You will be directed to the statistic Page.");
+                window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+        window.location.href = "http://127.0.0.1:9000/game/user/statistic";
+
+    }, 1000);
+
             }
         }, 1000);
         //to desplay and swap the bottons
         this.updateInterfaceControls();
-        console.log("hi");
     }
 
     stop(){
@@ -82,6 +69,7 @@ class Timer{
         clearInterval(this.interval);
         this.interval = null;
         this.updateInterfaceControls();
+
     }
 
     //method for returning the html string for inside the timer
@@ -92,9 +80,6 @@ class Timer{
         <span class="timer__part timer__part--seconds">00</span>
         <button type="button" class="timer__btn timer__btn--control timer__btn--start">
           <span class="material-icons">play_arrow</span>
-        </button>
-        <button type="button" class="timer__btn timer__btn--control timer__btn--reset">
-          <span class="material-icons">timer</span>
         </button>
         `
     }
